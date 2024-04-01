0cdd50e32a9d6b1d6a54e4c54152ae46c5bc76ec
Ravdeep Aulakh <vravdeep@gmail.com>
2024-03-31 17:18:05 -0700

    github actions setup

---
+++ Assignment3/Tests/EmployeeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NuGet.ContentModel;
+
+namespace Assignment3.Tests
+{
+    public class MyTests
+    {
+        [Fact]
+        public void TestAddition()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
+
